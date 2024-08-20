1a9d5cbe9893e6341b63bbdbb2305de4dd8dc640
Pat O'Connor <paoconno@redhat.com>
2024-08-20 09:39:06 +0100

    updated app
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express()
 
 // test
 app.get('/', (req, res) => res.send('Hello Tech Meetup!'))
-app.listen(3000, () => console.log('Server ready'))
+app.listen(3000, () => console.log('Server ready!'))
